547b9cedffdda3630202c494d2179f5aad8fa76a
mort607e <mortenmc2903@gmail.com>
2025-12-22 21:02:31 +0100

    Predict function added

--- riverseg/predict/predict.py
+++ riverseg/predict/predict.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from torch.nn import Module
 from riverseg.models.unet import UNet
 
@@ -21,12 +22,32 @@ def loadmodel(weight_path: str, device: str = "cpu", in_channels: int = 10, out_
     model.eval()
     return model
 
+def predict_river_extent(preprocessed_bands:np.ndarray, model: Module) -> np.ndarray:
+    """
+    Predicts the river extent from the preprocessed bands using the provided model.
+
+    Returns:
+    - numpy array with predicted river extent.
+    """ 
+    input_tensor = torch.from_numpy(preprocessed_bands).unsqueeze(0).float()
+    with torch.no_grad():
+        output = model(input_tensor)
+    predicted_mask = output.squeeze(0).cpu().numpy()
+    return predicted_mask
 
 if __name__ == "__main__":
+    # Initializing model
     weight_path = "weights\\weights.pth"
-
     model = loadmodel(weight_path, "cpu", 9, 1)
     print("Model loaded successfully!")
     print(f"Model architecture: {model.__class__.__name__}")
     print(f"Number of parameters: {sum(p.numel() for p in model.parameters())}")
     print(f"Device: {next(model.parameters()).device}")
+
+    # Prediction Example
+    preprocessed_bands = np.random.random((9,256,256)) # <- Relpace with the actual preprocessed bands.
+    predicted_mask = predict_river_extent(preprocessed_bands, model)
+    print("")
+    print("Prediction Example:")
+    print(f"Input bands shape: {preprocessed_bands.shape}")
+    print(f"Prediction shape: {predicted_mask.shape}")
